63b812ba1e6598c8b1e82d9814be6240c73cc484
XinYu <1173371605@qq.com>
2020-08-20 22:04:02 +0800

    纪念日

--- pagesA/anniversary/anniversary.vue
+++ pagesA/anniversary/anniversary.vue
@@ -1,5 +1,6 @@
 <template>
 	<view class="anniversary">
+		<!-- 盒子 -->
 		<view class="anniversary-box">
 			<view class="anniversary-box-title">我们已相爱</view>
 			<view class="anniversary-box-days">
@@ -12,6 +13,58 @@
 			</view>
 			<view class="anniversary-box-date">从2019-8-20至今</view>
 		</view>
+		<!-- 宝贝生日 -->
+		<view class="anniversary-baby baby">
+			<view class="anniversary-baby-birthday">
+				<view>10</view>
+				<view>1994.01</view>
+			</view>
+			<view class="anniversary-baby-text">宝贝潇潇的生日还有</view>
+			<view class="anniversary-baby-days">
+				<view>50</view>
+				<view>天</view>
+			</view>
+		</view>
+		<!-- 我的生日 -->
+		<view class="anniversary-baby">
+			<view class="anniversary-baby-birthday my">
+				<view>8</view>
+				<view>1993.05</view>
+			</view>
+			<view class="anniversary-baby-text">宝贝潇潇的生日还有</view>
+			<view class="anniversary-baby-days">
+				<view>50</view>
+				<view>天</view>
+			</view>
+		</view>
+		<!-- 第一次拥抱 -->
+		<view class="anniversary-embrace">
+			<view class="anniversary-embrace-left">
+				<image src="../../static/anniversary_3.png" mode=""></image>
+			</view>
+			<view class="anniversary-embrace-text">我们的第一次拥抱</view>
+			<view class="anniversary-embrace-right">
+				<image src="../../static/anniversary_5.png" mode=""></image>
+			</view>
+		</view>
+		<!-- 第一次接吻 -->
+		<view class="anniversary-embrace">
+			<view class="anniversary-embrace-left">
+				<image src="../../static/anniversary_4.png" mode=""></image>
+			</view>
+			<view class="anniversary-embrace-text">我们的第一次拥抱</view>
+			<view class="anniversary-embrace-right">
+				<image src="../../static/anniversary_5.png" mode=""></image>
+			</view>
+		</view>
+
+
+
+
+
+
+
+
 	</view>
 </template>
 
@@ -27,14 +80,14 @@
 
 <style lang="scss">
 	.anniversary {
-		height: 100vh;
+		padding-top: 30rpx;
+		height: calc(100vh - 30rpx);
 		background-image: linear-gradient(to bottom, #feecd6, #ffa9dc);
 	}
 
 	.anniversary-box {
-		position: relative;
-		top: 30rpx;
 		margin: 0 auto;
+		margin-bottom: 90rpx;
 		width: 532rpx;
 		height: 510rpx;
 		background-image: url(../../static/anniversary_0.png);
@@ -96,5 +149,110 @@
 		position: relative;
 		top: -60rpx;
 		color: #aeaeae;
+	}
+
+	// 宝贝生日
+	.anniversary-baby {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin: 0 auto;
+		margin-top: 40rpx;
+		width: 90%;
+		height: 104rpx;
+		background-color: #FFFFFF;
+		border-radius: 50rpx;
+	}
+
+	.anniversary-baby-birthday {
+		margin-left: -10rpx;
+		width: 105rpx;
+		height: 105rpx;
+		color: #FFFFFF;
+		text-align: center;
+		font-size: 20rpx;
+		background-color: #ffaa3e;
+		border-radius: 50%;
+
+		:nth-child(1) {
+			font-size: 50rpx;
+			height: 60rpx;
+			padding-top: 4rpx;
+		}
+	}
+
+	.my {
+		background-color: #a743fd;
+	}
+
+	.anniversary-baby-text {
+		text-align: left;
+		width: 400rpx;
+	}
+
+	.anniversary-baby-days {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100rpx;
+		height: 100rpx;
+		background-color: #FFFFFF;
+		border-radius: 50%;
+		margin-right: 20rpx;
+
+		:nth-child(1) {
+			font-size: 50rpx;
+		}
+
+		:nth-child(2) {
+			font-size: 35rpx;
+		}
+	}
+
+	// 第一次
+	.anniversary-embrace {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		margin: 0 auto;
+		margin-top: 40rpx;
+		width: 90%;
+		height: 104rpx;
+		background-color: #FFFFFF;
+		border-radius: 50rpx;
+	}
+
+	.anniversary-embrace-left {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100rpx;
+		height: 100rpx;
+		border-radius: 50%;
+
+		image {
+			width: 50rpx;
+			height: 60rpx;
+		}
+	}
+
+	.anniversary-embrace-right {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		width: 100rpx;
+		height: 100rpx;
+		border-radius: 50%;
+		box-shadow: 0rpx 0rpx 10rpx #e6e6e6;
+
+		image {
+			width: 50rpx;
+			height: 60rpx;
+		}
+	}
+
+	.anniversary-embrace-text {
+		text-align: left;
+		width: 400rpx;
 	}
 </style>
--- pagesA/myLove/myLove.vue
+++ pagesA/myLove/myLove.vue
@@ -6,7 +6,7 @@
 				<view class="follow-list-left_name">
 					<view class="follow-list-left_nametext1">作者:潇潇</view>
 					<view class="follow-list-left_nametext2">关于她</view>
-					<view class="follow-list-left_nametext3">坎坎啦啦啦啦坷坷刚刚看完了一段韩剧，里面有一个评论是这样说的</view>
+					<view class="follow-list-left_nametext3">坎坎啦啦啦啦坷坷刚刚啦啦啦坷坷刚刚啦啦啦坷坷刚刚啦啦啦坷坷刚刚啦啦啦坷坷刚刚看完了一段韩剧，里面有一个评论是这样说的</view>
 				</view>
 			</view>
 			<view class="follow-list-right">
@@ -48,9 +48,12 @@
 
 	.follow-list-left {
 		display: flex;
+		width: 70%;
 	}
 
 	.follow-list-right {
+		width: 30%;
+		text-align: -webkit-right;
 		image {
 			display: flex;
 			justify-content: center;
@@ -67,32 +70,30 @@
 	.follow-list-left_name {
 		padding-left: 20rpx;
 		color: #000000;
+		width: 100%;
 	}
 
 	.follow-list-left_nametext1 {
-		font-size: 30rpx;
+		font-size: 25rpx;
 		margin-top: 10rpx;
 		color: #c5c5c5;
 	}
 
 	.follow-list-left_nametext2 {
-		font-size: 30rpx;
+		font-size: 20rpx;
 		margin-top: 10rpx;
 		color: #000000;
-		text-overflow: -o-ellipsis-lastline;
+	}
+
+	.follow-list-left_nametext3 {
+		margin-top: 10rpx;
+		font-size: 28rpx;
+		height: 80rpx;
 		overflow: hidden;
 		text-overflow: ellipsis;
 		display: -webkit-box;
 		-webkit-line-clamp: 2;
-		line-clamp: 2;
+		line-clamp: 3;
 		-webkit-box-orient: vertical;
-	}
-
-	.follow-list-left_nametext3 {
-		font-size: 30rpx;
-		margin-top: 10rpx;
-		color: #c5c5c5;
-		width: 90%;
-		overflow: hidden;
 	}
 </style>
